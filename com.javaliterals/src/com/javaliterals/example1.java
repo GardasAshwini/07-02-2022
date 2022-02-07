5e5a895a54d8d9c7200eb3614474808c5cac0e78
aman gardas <aman gardas@DESKTOP-AQAT1N6>
2022-02-07 11:57:19 +0530

    com.javaliterals added into com.javaliterals

---
+++ com.javaliterals/src/com/javaliterals/example1.java
@@ -0,0 +1,37 @@
+package com.javaliterals;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// Write a java program to student information(id,name,age,marks,grade,result,address) using java literals
+		
+		int studentId =123;
+		String studentName = "Ashwini";
+		int studentAge = 21;
+		float studentMarks = 99.99f;
+		String studentGrade = "A";
+		String studentResult = "Pass";
+		String studentAddress = "Kamareddy";
+		System.out.print("Student Id: ");
+		System.out.println(studentId);
+		
+		System.out.print("Student Name: ");
+		System.out.println(studentName);
+		
+		System.out.print("Student Age: ");
+		System.out.println(studentAge);
+
+		System.out.print("Student Marks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("Student Grade: ");
+		System.out.println(studentGrade);
+		
+		System.out.print("Student Result: ");
+		System.out.println(studentResult);
+		
+		System.out.print("Student Address ");
+		System.out.println(studentAddress);
+	}
+
+}
